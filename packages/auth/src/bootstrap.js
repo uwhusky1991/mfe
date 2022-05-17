d545cb1519c2705d9c58a574c0378c86c40c19af
uwhusky1991 <rob_fink@hotmail.com>
2022-05-17 09:57:36 -0700

    auth

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -26,7 +26,7 @@ const mount = (el, { onNavigate, defaultHistory }) => {
 // If we are in development and in isolation,
 // call mount immediately
 if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#_marketing-dev-root');
+  const devRoot = document.querySelector('#_auth-dev-root');
 
   if (devRoot) {
     mount(devRoot, { defaultHistory: createBrowserHistory() });
